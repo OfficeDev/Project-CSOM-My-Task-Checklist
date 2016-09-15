dade32ddf61a33f8e5e61dd39763cb7de2ae13df
namerali <namerali@NAMERALI_MAIN>
2016-09-15 13:57:55 -0700

    Initial Commit

---
+++ Project-CSOM-My-Task-Checklist/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_CSOM_My_Task_Checklist
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMyCheckList());
+        }
+    }
+}
